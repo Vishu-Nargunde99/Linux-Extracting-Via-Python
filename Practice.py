e2b25b7e1199f449ac30283bf87365082ba799ab
Vishakha Nargunde <vishakhanargunde1999@gmail.com>
2025-08-25 19:00:42 +0530

    chnages made

--- Practice.py
+++ Practice.py
@@ -1,1 +1,47 @@
-pip 
+'''import platform 
+
+# printing the Architecture of the OS
+print("[+] Architecture:", platform.architecture()[0])
+
+# printing the Operating System release information
+print("[+] Operating System Release :", platform.release())
+
+# prints the currently using system name
+print("[+] System Name :",platform.system())
+
+# This line will print the version of your Operating System
+print("[+] Operating System Version :", platform.version())
+
+# This will print the Node or hostname of your Operating System
+print("[+] Node: " + platform.node())
+
+# This will print your system platform
+print("[+] Platform :", platform.platform())
+
+# This will print the processor information
+print("[+] Processor :",platform.processor())
+print("[+] MAchne:", platform.machine)
+# printing the Operating System release information
+print("[+] Operating System Release :", platform.release())
+
+# prints the currently using system name
+print("[+] System Name :",platform.system())
+
+# This line will print the version of your Operating System
+print("[+] Operating System Version :", platform.version())
+
+# This will print the Node or hostname of your Operating System
+print("[+] Node: " + platform.node())
+
+# This will print your system platform
+print("[+] Platform :", platform.platform())
+
+# This will print the processor information
+print("[+] Processor :",platform.processor())
+'''
+from datetime import datetime
+import psutil
+
+# Using the psutil library to get the boot time of the system
+boot_time = datetime.fromtimestamp(psutil.boot_time())
+print("[+] System Boot Time :", boot_time)
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# Linux-Extracting-Via-Python
+# Extracting Linux System and Hardware Info Using Python 
+import platform
+
+# printing the Architecture of the OS
+print("[+] Architecture :", platform.architecture()[0])
+
+# Displaying the machine
+print("[+] Machine :", platform.machine())
+
+# printing the Operating System release information
+print("[+] Operating System Release :", platform.release())
+
+# prints the currently using system name
+print("[+] System Name :",platform.system())
+
+# This line will print the version of your Operating System
+print("[+] Operating System Version :", platform.version())
+
+# This will print the Node or hostname of your Operating System
+print("[+] Node: " + platform.node())
+
+# This will print your system platform
+print("[+] Platform :", platform.platform())
+
+# This will print the processor information
+print("[+] Processor :",platform.processor())
