1cd38d10403b28050fb808bbf69dc407cc10be04
Vishakha Nargunde <vishakhanargunde1999@gmail.com>
2025-08-26 11:46:09 +0530

    chnages

--- Practice.py
+++ Practice.py
@@ -104,13 +104,15 @@ for i, percentage in enumerate(psutil.cpu_percent(percpu=True, interval=1)):
 print(f"[+] Total CPU Usage : {psutil.cpu_percent()}%")
 
 
-'''
+
 
 # reading the cpuinfo file to print the name of
 # the CPU present
 with open("/proc/cpuinfo", "r") as f:
     file_info = f.readlines()
-
+    
 cpuinfo = [x.strip().split(":")[1] for x in file_info if "model name" in x]
 for index, item in enumerate(cpuinfo):
     print("[+] Processor " + str(index) + " : " + item)
+    
+'''
